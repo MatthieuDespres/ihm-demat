147f180b50135341ef2ce463eccd31a728b28878
Matthieu Despres <despres.matthieu@gmail.com>
2024-05-22 22:26:58 +0200

    add first lines js

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
 const display = document.querySelector(".display")
-
-const mainMenu = {
-    title: "Menu",
-    childItems : ["Consommations", "Absence", "Boost", "Gestion Consignes", "Paramétrage"],
-    itemDefaultFocus: 2,
-    itemFocus: 2,
-
-    show(){
-        const menuTitle = document.createElement("h2")
-        menuTitle.classList.add("menu-title")
-        menuTitle.textContent = this.title
-        const list = document.createElement("ul")
-        list.classList.add("menu-list")
-        for (let index = 0; index < this.childItems.length; index++) {
-            const listItem = document.createElement("li")
-            listItem.classList.add("menu-element")
-            if (index === this.itemFocus) {
-                listItem.classList.add("selected")
-            }
-            listItem.textContent = this.childItems[index]
-            list.appendChild(listItem)
-        }
-        display.textContent = ""
-        display.appendChild(menuTitle)
-        display.appendChild(list)
-    }
-}
-// mainMenu.show()
-
 const menuBtn = document.querySelector(".btn-menu")
-menuBtn.addEventListener("click", () => {
-    mainMenu.show()
-})
+menuBtn.addEventListener("click", openMainMenu)
+
+function openMainMenu() {
+    const data = [{title: "Consommations"}, {title: "Absence"}, {title: "Boost"}, {title: "Gestion Consigne"}, {title: "Paramètres"}]
+
+    const mainMenuTitle = document.createElement("h2")
+    mainMenuTitle.classList.add("menu-title")
+    mainMenuTitle.textContent = "Menu"
+    const listItems = document.createElement("ul")
+    listItems.classList.add("menu-list")
+
+    data.forEach( item => {
+        const listItem = document.createElement("li")
+        listItem.classList.add("menu-element")
+        listItem.textContent = item.title
+        listItems.appendChild(listItem)
+    })
+
+
+    clearDisplay()
+    display.appendChild(mainMenuTitle)
+    display.appendChild(listItems)
+
+}
+
+function clearDisplay() {
+    display.textContent = ""
+}
 
 const backBtn = document.querySelector(".btn-back")
-backBtn.addEventListener("click", () => {
-    display.textContent = ""
-    display.innerHTML = '<h2 class="menu-title">Accueil</h2>'
-})
+backBtn.addEventListener("click", openHomPage)
+function openHomPage() {
+    const homeTitle = document.createElement("h2")
+    homeTitle.classList.add("home-title")
+    homeTitle.textContent = "Ven 11:23"
+
+    const waterDromIMG = document.createElement("img")
+    waterDromIMG.src = "./Ressources/water-drop.svg"
+    waterDromIMG.classList.add("water-drop")
+    waterDromIMG.alt = "Goutte d'eau"
+
+
+    clearDisplay()
+    display.appendChild(homeTitle)
+    display.appendChild(waterDromIMG)
 
 
 
 
-class Discover {
-    display = document.querySelector(".display")
-
-
-
-    static show(item) {
-        console.log(item);
-    }
 }
