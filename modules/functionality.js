ab4938d5de0245e9912d8ae5b8b628c97165e9ba
Matthieu Despres <despres.matthieu@gmail.com>
2024-06-02 16:30:00 +0200

    remove steps

--- modules/functionality.js
+++ modules/functionality.js
@@ -1,21 +1,14 @@
 class Functionality {
     title
-    steps = []
-
-    constructor(title, steps) {
+    constructor(title) {
         this.title = title
-        this.steps = steps
     }
-
     getShowElements(){
         const elementsToShow = []
         const menuTitle = document.createElement("h2")
         menuTitle.classList.add("menu-title")
         menuTitle.textContent = this.title
         elementsToShow.push(menuTitle)
-        if(this.steps) {
-            this.steps.forEach(step => step.elements.forEach(element => elementsToShow.push(element)))
-        }
         return elementsToShow
     }
     mooveUp(){
