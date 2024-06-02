51f958010bb86ab25661126f64bfd3338c26e180
Matthieu Despres <despres.matthieu@gmail.com>
2024-06-02 15:59:05 +0200

    final version manu and functionality

--- modules/functionality.js
+++ modules/functionality.js
@@ -1,9 +1,4 @@
-/*
-    Class Functionality :
-    Manage the functionality
-*/
 class Functionality {
-    // Title which is written at the top of the functionality
     title
     steps = []
 
@@ -12,7 +7,6 @@ class Functionality {
         this.steps = steps
     }
 
-    // Return the html elements to show
     getShowElements(){
         const elementsToShow = []
         const menuTitle = document.createElement("h2")
@@ -24,38 +18,13 @@ class Functionality {
         }
         return elementsToShow
     }
-    // When we move the cursor on the top
     mooveUp(){
     }
-
-    // When we move the cursor on the bottom
     mooveDown(){
     }
     ok(){
     }
     back(){
-
     }
 }
 export default Functionality
-
-
-
-/* DOM Home */
-// const homeTitle = document.createElement("h2")
-// homeTitle.classList.add("home-title")
-// homeTitle.textContent = "Ven 11:23"
-
-// const waterDromIMG = document.createElement("img")
-// waterDromIMG.src = "./Ressources/water-drop.svg"
-// waterDromIMG.classList.add("water-drop")
-// waterDromIMG.alt = "Goutte d'eau"
-
-
-// discover.clearDisplay()
-// discover.display.appendChild(homeTitle)
-// discover.display.appendChild(waterDromIMG)
-
-
-
-/* Ajouter la fgeature d'alerte avant le menu expert */
--- modules/menu.js
+++ modules/menu.js
@@ -1,28 +1,14 @@
-/*
-    Class Menu :
-    Manage the Menu
-*/
 class Menu {
-    // Title which is written at the top of the menu
     title
-
-    // Array containing the functionalities or submenus
     childsItems
-
-    // Element selected by default
     itemSelectedByDefault
-
-    // Currently selected item
     selectedItem
-
     constructor(title, childsItems, itemSelectedByDefault) {
         this.title = title
         this.childsItems = childsItems
         this.itemSelectedByDefault = itemSelectedByDefault
         this.selectedItem = this.itemSelectedByDefault
     }
-
-    // Return the html elements to show
     getShowElements(){
         const menuTitle = document.createElement("h2")
         menuTitle.classList.add("menu-title")
@@ -45,30 +31,22 @@ class Menu {
         }
         return [menuTitle, menuContainer]
     }
-
-    // When we move the cursor on the top
     mooveUp(){
         if(this.selectedItem > 0 ){
             this.selectedItem -= 1
         }
     }
-
-    // When we move the cursor on the bottom
     mooveDown(){
         if(this.selectedItem < (this.childsItems.length - 1)){
             this.selectedItem += 1
         }
     }
-    // When whant to acces on sub menu
     ok(){
         return this.childsItems[this.selectedItem]
     }
-    //When want to acces to the parent menu
     back() {
-        // Reset the selected item from the origin
         this.resetSelectedItem()
     }
-    // When we go to the parent menu or the home page we want reset de default position of selector
     resetSelectedItem() {
         this.selectedItem = this.itemSelectedByDefault
     }
